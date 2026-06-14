3ac581c3a251da5905a6b1059249107d644dee6d
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-06-14 03:35:03 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = https://nwzlbxenuiznaydxhhso.supabase.co;
+const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
