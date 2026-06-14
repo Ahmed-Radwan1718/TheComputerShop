8b7bec7b5a5cc5d138060698e8fdcd8df09ab74c
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-06-14 16:00:13 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = https://nwzlbxenuiznaydxhhso.supabase.co;
-const SUPABASE_PUBLISHABLE_KEY = sb_publishable_FKLvX-BMUIwXgv9PKwryRQ_Si9-lkjp;
+const SUPABASE_URL = "https://nwzlbxenuiznaydxhhso.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_FKLvX-BMUIwXgv9PKwryRQ_Si9-lkjp";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
