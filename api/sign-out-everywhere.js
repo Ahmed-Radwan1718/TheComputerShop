fc8bf23fff16b8e761c1e5d8774eafb644fc1b3e
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-06-21 14:32:59 +0300

    Create sign-out-everywhere.js

---
+++ api/sign-out-everywhere.js
@@ -0,0 +1,28 @@
+const admin = require("./_lib/firebaseAdmin");
+
+const {
+  getUserFromRequest
+} = require("./_lib/securityHelpers");
+
+module.exports = async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const decodedUser = await getUserFromRequest(req);
+
+    await admin.auth().revokeRefreshTokens(decodedUser.uid);
+
+    const db = admin.firestore();
+
+    await Promise.all([
+      db.collection("securityPasswordSessions").doc(decodedUser.uid).delete().catch(function () {}),
+      db.collection("securityPasswordCodes").doc(decodedUser.uid).delete().catch(function () {})
+    ]);
+
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    return res.status(500).json({ error: "Could not sign out from all devices." });
+  }
+};
