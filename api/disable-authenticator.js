a589470f9087e662b14774df38fbb74271d3c670
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-06-23 00:29:13 +0300

    Update disable-authenticator.js

--- api/disable-authenticator.js
+++ api/disable-authenticator.js
@@ -10,7 +10,10 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    const decodedUser = await getUserFromRequest(req);
+const decodedUser = await getUserFromRequest(req, {
+  checkRevoked: true,
+  requireCompletedTwoFactor: true
+});
     const db = admin.firestore();
     const userRef = db.collection("users").doc(decodedUser.uid);
     const userDoc = await userRef.get();
