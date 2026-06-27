1ff156a7ac4671c49bb605ae5489c85cac3a46a7
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-06-27 18:58:04 +0300

    Update verify-login-authenticator.js

--- server/routes/verify-login-authenticator.js
+++ server/routes/verify-login-authenticator.js
@@ -78,10 +78,10 @@ async function clearFailures(uid) {
 
 async function createCompatibleSiteSession(req, res, challenge) {
   if (challenge.idToken && typeof createSiteSessionFromIdToken === "function") {
-    return await createSiteSessionFromIdToken(challenge.idToken, res);
+    return await createSiteSessionFromIdToken(challenge.idToken, res, req);
   }
 
-  return await createSiteSessionForUid(challenge.uid, res);
+  return await createSiteSessionForUid(challenge.uid, res, req);
 }
 
 async function createCompatibleTwoFactorSession(req, res, uid) {
