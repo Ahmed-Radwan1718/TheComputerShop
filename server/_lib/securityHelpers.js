f109137d8d3421f98aa1dc12b10e0f2fffc08f93
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-07-19 01:39:36 +0300

    Update securityHelpers.js

--- server/_lib/securityHelpers.js
+++ server/_lib/securityHelpers.js
@@ -4,25 +4,11 @@ const admin = require("./firebaseAdmin");
 const IS_PRODUCTION =
   process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "production";
 
-const SITE_SESSION_COOKIE_NAME = IS_PRODUCTION
-  ? "__Host-tcs_session"
-  : "tcs_session";
-
-const LOGIN_CHALLENGE_COOKIE_NAME = IS_PRODUCTION
-  ? "__Host-tcs_login_challenge"
-  : "tcs_login_challenge";
-
-const LOGIN_2FA_COOKIE_NAME = IS_PRODUCTION
-  ? "__Host-tcs_login_2fa"
-  : "tcs_login_2fa";
-
-const ACCOUNT_SESSION_COOKIE_NAME = IS_PRODUCTION
-  ? "__Host-tcs_account_session"
-  : "tcs_account_session";
-
-const TRUSTED_DEVICE_COOKIE_NAME = IS_PRODUCTION
-  ? "__Host-tcs_trusted_device"
-  : "tcs_trusted_device";
+const SITE_SESSION_COOKIE_NAME = "tcs_session";
+const LOGIN_CHALLENGE_COOKIE_NAME = "tcs_login_challenge";
+const LOGIN_2FA_COOKIE_NAME = "tcs_login_2fa";
+const ACCOUNT_SESSION_COOKIE_NAME = "tcs_account_session";
+const TRUSTED_DEVICE_COOKIE_NAME = "tcs_trusted_device";
 
 const SITE_SESSION_EXPIRES_MS = 5 * 24 * 60 * 60 * 1000;
 const ACCOUNT_SESSION_EXPIRES_MS = SITE_SESSION_EXPIRES_MS;
