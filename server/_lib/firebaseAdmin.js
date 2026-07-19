beaced23fd08c829f2e4398e5e30b46571bae1e8
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-07-19 17:55:47 +0300

    Update firebaseAdmin.js

--- server/_lib/firebaseAdmin.js
+++ server/_lib/firebaseAdmin.js
@@ -1,19 +1,13 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  const firebaseOptions = {
+  admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     })
-  };
-
-  if (process.env.FIREBASE_DATABASE_URL) {
-    firebaseOptions.databaseURL = process.env.FIREBASE_DATABASE_URL;
-  }
-
-  admin.initializeApp(firebaseOptions);
+  });
 }
 
 module.exports = admin;
